d78795b8fb23a0219849076d63167d0c0c0a3f60
JedWong <jiede2011@hotmail.com>
2025-04-11 09:12:49 +0800

    1

--- setup.py
+++ setup.py
@@ -2,17 +2,27 @@ from setuptools import setup, find_packages
 
 setup(
     name="xyzs_py",  # 包名，建议与项目文件夹名称一致
-    version="0.1.0",  # 版本号，遵循语义化版本控制
+    version="1.0.0",  # 版本号，遵循语义化版本控制
     author="Jed",  # 作者
     author_email="jiede2011@hotmail.com",  # 作者邮箱
     description="封装Mysql、Redis、时间操作类、yaml配置操作、Http操作、日志操作等等快速使用的代码",  # 包的简要描述
     long_description=open("README.md").read(),  # 长描述，一般从 README.md 文件中读取
     long_description_content_type="text/markdown",  # 长描述的格式，通常为 Markdown
-    url="https://your-git-url/your_project",  # 项目的主页或 Git 仓库 URL
+    url="https://github.com/JedTK/xyzs_py",  # 项目的主页或 Git 仓库 URL
     packages=find_packages(),  # 自动查找项目中的所有包
+    # 在这里列出依赖包及其版本
     install_requires=[
-        "requests>=2.25.1",  # 在这里列出依赖包及其版本
-        "numpy>=1.19.0"
+        "requests~=2.32.3",
+        "colorlog~=6.8.2",
+        "aiortc~=1.9.0",
+        "certifi~=2024.7.4",
+        "aiohttp~=3.10.11",
+        "setuptools~=70.1.1",
+        "PyYAML~=6.0.1",
+        "redis~=5.0.8",
+        "APScheduler~=3.10.4",
+        "SQLAlchemy~=2.0.36",
+        "DateTime~=5.5",
     ],
     classifiers=[  # 分类信息
         "Programming Language :: Python :: 3",
