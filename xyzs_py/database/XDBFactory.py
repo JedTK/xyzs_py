647a318dbea79ce97eabdf58a798ab7af872bf04
JedWong <jiede2011@hotmail.com>
2025-09-18 11:55:36 +0800

    Update XDBFactory.py

--- xyzs_py/database/XDBFactory.py
+++ xyzs_py/database/XDBFactory.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from threading import RLock
-from typing import Dict, Optional, Iterable
+from typing import Dict, Optional, Iterable, Callable
 
 from xyzs_py.database import XDBConnect
 from xyzs_py.database.XAsyncDBConnect import XAsyncDBConnect
@@ -33,20 +33,41 @@ class XDBFactory:
     _bundles: Dict[str, DBBundle] = {}
     _lock = RLock()
 
+    __register_main_db_Listener = None
+    __register_slave_DB_Listener = None
+
+    @classmethod
+    def register_main_db(cls, register_main_db_Listener):
+        """
+        注入主库注册监听器，允许调用者自主管理主库连接信息的注册。
+        通常在应用启动时注入但不激活，待实际需要数据库连接时才触发注册。
+        :param register_main_db_Listener: 主库监听器
+        """
+        cls.__register_main_db_Listener = register_main_db_Listener
+
+    @classmethod
+    def register_slave_db(cls, register_slave_DB_Listener):
+        """
+        注入注册从库监听器，允许调用者自主管理主库连接信息的注册。
+        通常在应用启动时注入但不激活，待实际需要数据库连接时才触发注册。
+        :param register_slave_DB_Listener: 从库监听器
+        """
+        cls.__register_slave_DB_Listener = register_slave_DB_Listener
+
     @classmethod
     def register(cls,
-                 key: str = "default",
+                 key: str = "main",
                  write_connect: Optional[XDBConnect] = None,
                  read_connect: Optional[XDBConnect] = None,
                  write_async_connect: Optional[XAsyncDBConnect] = None,
                  read_async_connect: Optional[XAsyncDBConnect] = None,
-                 overwrite: bool = True) -> None:
+                 overwrite: bool = False) -> None:
         """
-        注册数据库连接：
+        注册数据库连接，支持同步/异步配置，以业务 key 分组存储。
         - 可只传同步（write_connect+read_connect）
         - 可只传异步（write_async_connect+read_async_connect）
         - 也可两者都传
-        overwrite=True：重复注册会覆盖对应部分；False：若已存在则抛异常
+        overwrite=True：重复注册会覆盖对应部分；False：若已存在则直接返回不进行覆盖
         """
         if not any([write_connect and read_connect, write_async_connect and read_async_connect]):
             raise ValueError("register 需要同时提供同步写/读 或 异步写/读 中的至少一组。")
@@ -59,7 +80,7 @@ class XDBFactory:
                 if not (write_connect and read_connect):
                     raise ValueError("同步注册需要同时提供 write_connect 和 read_connect。")
                 if bundle.sync is not None and not overwrite:
-                    raise ValueError(f"Key='{key}' 已存在同步管理器，且 overwrite=False。")
+                    return
                 bundle.sync = XDBManager(write_connect=write_connect, read_connect=read_connect)
 
             # 异步
@@ -67,17 +88,23 @@ class XDBFactory:
                 if not (write_async_connect and read_async_connect):
                     raise ValueError("异步注册需要同时提供 write_async_connect 和 read_async_connect。")
                 if bundle.async_ is not None and not overwrite:
-                    raise ValueError(f"Key='{key}' 已存在异步管理器，且 overwrite=False。")
+                    return
                 bundle.async_ = XAsyncDBManager(write_connect=write_async_connect,
                                                 read_connect=read_async_connect)
-
             cls._bundles[key] = bundle.ensure_any()
 
-    # —— 获取接口 —— #
+    # region remark - 获取同步 DB 管理器
+
     @classmethod
-    def get_sync_db(cls, key: str = "default", *, required: bool = True) -> Optional[XDBManager]:
+    def get_sync_db(cls, key: str = "main", *, required: bool = True) -> Optional[XDBManager]:
         """获取同步 DB 管理器。required=False 时若不存在返回 None。"""
         with cls._lock:
+            if key not in cls._bundles:
+                if key == "main" and cls.__register_main_db_Listener:
+                    cls.__register_main_db_Listener(key)  # 触发监听器注册，用户自行实现注册逻辑，最终会调用 register() 方法进行注册
+                elif cls.__register_slave_DB_Listener:
+                    cls.__register_slave_DB_Listener(key)
+
             bundle = cls._bundles.get(key)
             if not bundle or not bundle.sync:
                 if required:
@@ -85,10 +112,19 @@ class XDBFactory:
                 return None
             return bundle.sync
 
+    #  endregion
+
+    #  region remark - 获取异步 DB 管理器
     @classmethod
-    def get_async_db(cls, key: str = "default", *, required: bool = True) -> Optional[XAsyncDBManager]:
+    def get_async_db(cls, key: str = "main", *, required: bool = True) -> Optional[XAsyncDBManager]:
         """获取异步 DB 管理器（注意：这不是 async 函数）。"""
         with cls._lock:
+            if key not in cls._bundles:
+                if key == "main" and cls.__register_main_db_Listener:
+                    cls.__register_main_db_Listener(key)  # 触发监听器注册，用户自行实现注册逻辑，最终会调用 register() 方法进行注册
+                elif cls.__register_slave_DB_Listener:
+                    cls.__register_slave_DB_Listener(key)
+
             bundle = cls._bundles.get(key)
             if not bundle or not bundle.async_:
                 if required:
@@ -96,7 +132,9 @@ class XDBFactory:
                 return None
             return bundle.async_
 
-    # —— 维护/工具接口 —— #
+    # endregion
+
+    # region remark - 维护/工具接口
     @classmethod
     def has_key(cls, key: str) -> bool:
         with cls._lock:
@@ -138,3 +176,4 @@ class XDBFactory:
     def _not_found_msg(cls, key: str, want: str) -> str:
         keys = ", ".join(cls._bundles.keys()) or "<无>"
         return f"[XDBFactory] 未找到 {want} 数据库连接: '{key}'。可用 keys = {keys}"
+    # endregion
